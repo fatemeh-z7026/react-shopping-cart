90b67873af0c8c0acb86844a58abb4bfc5af05a1
F-zeinali <fatemeh.zeinali7026@gmail.com>
2024-09-13 15:41:42 +0330

    initial commit

--- shoppingcart/src/components/Shop/CartProducts/CartProducts.js
+++ shoppingcart/src/components/Shop/CartProducts/CartProducts.js
@@ -1,31 +1,34 @@
 // CartProduct.js
-import React from "react";
 import "./CartProducts.css"; 
+import React, { Component } from 'react'
 
-const CartProducts = () => {
-  return (
-    <div class="cart-container">
-    <h1>Your Cart</h1>
-    <ul class="cart-items">
-        <li class="cart-item">
-            <span class="item-name">Product 1</span>
-            <span class="item-price">$10.00</span>
-            <button class="remove-item">Remove</button>
-        </li>
-        <li class="cart-item">
-            <span class="item-name">Product 2</span>
-            <span class="item-price">$15.00</span>
-            <button class="remove-item">Remove</button>
-        </li>
-        <li class="cart-item">
-            <span class="item-name">Product 3</span>
-            <span class="item-price">$20.00</span>
-            <button class="remove-item">Remove</button>
-        </li>
-    </ul>
-    <button class="remove-all">Remove All Products</button>
-</div>
-  );
-};
+export default class CartProducts extends Component {
+  render() {
+    return (
+        <div class="cart-container">
+        <h1>Your Cart</h1>
+        <ul class="cart-items">
+            <li class="cart-item">
+                <span class="item-name">Product 1</span>
+                <span class="item-price">$10.00</span>
+                <button class="remove-item">Remove</button>
+            </li>
+            <li class="cart-item">
+                <span class="item-name">Product 2</span>
+                <span class="item-price">$15.00</span>
+                <button class="remove-item">Remove</button>
+            </li>
+            <li class="cart-item">
+                <span class="item-name">Product 3</span>
+                <span class="item-price">$20.00</span>
+                <button class="remove-item">Remove</button>
+            </li>
+        </ul>
+        <button class="remove-all">Remove All Products</button>
+    </div>
+    )
+  }
+}
 
-export default CartProducts;
+
+
--- shoppingcart/src/components/Shop/Products/Products.js
+++ shoppingcart/src/components/Shop/Products/Products.js
@@ -1,18 +1,16 @@
 // Product.js
-import React from "react";
 import "./Products.css";
+import React, { Component } from "react";
 
-const Products = () => {
-  return (
-    
+export default class Products extends Component {
+  render() {
+    return (
       <div class="product">
         <img src="Image/Shirt.png" alt="Product " class="product-image" />
         <h2 class="product-title">Sample Product</h2>
         <p class="product-price">$19.99</p>
         <button class="add-button">Add to Cart</button>
       </div>
-   
-  );
-};
-
-export default Products;
+    );
+  }
+}
